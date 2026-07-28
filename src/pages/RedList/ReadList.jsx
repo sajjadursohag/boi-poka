f7b1d1f07e018b8499e2469b3582ef26345fedbb
sajjadursohag <sajjadursub07@gmail.com>
2026-07-29 02:00:58 +0600

    added sort functionality

--- src/pages/RedList/ReadList.jsx
+++ src/pages/RedList/ReadList.jsx
@@ -10,9 +10,9 @@ const ReadList = () => {
   // database diye korle onk easy hobe.
 
   const [readList, setReadList] = useState([]);
+  const [sort, setSort] = useState("");
 
   const data = useLoaderData();
-  console.log(data);
 
   useEffect(() => {
     const storedBookData = getStoredBook();
@@ -23,8 +23,34 @@ const ReadList = () => {
     setReadList(myReadList);
   }, []);
 
+  const handleSort = (type) => {
+    setSort(type);
+    if (type === "pages") {
+      const sortedByPage = [...readList].sort(
+        (a, b) => a.totalPages - b.totalPages,
+      );
+      setReadList(sortedByPage);
+      console.log(sortedByPage);
+    }
+    if (type === "ratings") {
+      const sortedByrating = [...readList].sort((a, b) => a.rating - b.rating);
+      setReadList(sortedByrating);
+    }
+  };
+
   return (
     <div>
+      <details className="dropdown">
+        <summary className="btn m-1">Sort by: {sort ? sort : ""}</summary>
+        <ul className="menu dropdown-content bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+          <li>
+            <a onClick={() => handleSort("pages")}>pages</a>
+          </li>
+          <li>
+            <a onClick={() => handleSort("ratings")}>ratings</a>
+          </li>
+        </ul>
+      </details>
       <Tabs>
         <TabList>
           <Tab>Read Book List</Tab>
@@ -34,9 +60,9 @@ const ReadList = () => {
         <TabPanel>
           <h2>Book i read {readList.length}</h2>
 
-          {
-            readList.map(b=><Book key={b.bookId} singleBook={b}></Book>)
-          }
+          {readList.map((b) => (
+            <Book key={b.bookId} singleBook={b}></Book>
+          ))}
         </TabPanel>
         <TabPanel>
           <h2>My Wish List</h2>
